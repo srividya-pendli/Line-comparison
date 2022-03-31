867573ee2d4c01852ec76ba096f64bff046ae8e9
srividya-pendli <srividyaerukala@gmail.com>
2022-03-31 22:31:03 +0530

    Add UC2 branch and programs.cs

--- Line comaprison/Program.cs
+++ Line comaprison/Program.cs
@@ -12,13 +12,22 @@ namespace Line_comaprison
         {
             Console.WriteLine(" Welcome to line comparison");
             {
-                double x1, y1, x2, y2, length;
+                double x1, y1, x2, y2, x3, y3, x4, y4, length1, length2;
                 x1 = 3;
                 y1 = 5;
                 x2 = 8;
                 y2 = 9;
-                length = Math.Sqrt(Math.Abs((x2 - x1) * (x2 - x1) + (x2 - x1) + (y2 - y1) * (y2 - y1) * (y2 = y1)));
-                Console.WriteLine("Length of two points is:{0}", length);
+                x3 = 5;
+                y3 = 6;
+                x4 = 7;
+                y4 = 4;
+                length1 = Math.Sqrt(Math.Abs((x2 - x1) * (x2 - x1) + (x2 - x1) + (y2 - y1) * (y2 - y1) * (y2 = y1)));
+                length2 = Math.Sqrt(Math.Abs((x4 - x3) * (x4 - x3) + (x4 - x3) + (y4 - y3) * (y4 - y3) * (y4 = y3)));
+                Console.WriteLine("Length of first line is:{0} and second line is:{1}", length1,length2);
+                if (length1 == length2)
+                    Console.WriteLine("Two lines are equal");
+                else
+                    Console.WriteLine("Two lines are not equal");
                 Console.ReadLine();
             }
         }
